24455d5145114801baf2f4504de8241c9141e4a2
Iyad Ahmed <iyadahmed430@gmail.com>
2023-03-17 17:28:01 +0200

    Print timing of calculation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+from time import perf_counter
 
 # Check if "cython" appears in command-line arguments that come after the Python file name
 if "cython" in sys.argv[1:]:
@@ -8,4 +9,7 @@ else:
     from gen_primes import count_primes_less_than
 
 if __name__ == "__main__":
+    t1 = perf_counter()
     print(count_primes_less_than(10000000))
+    t2 = perf_counter()
+    print(f"Calculation took {t2 - t1} seconds.")
